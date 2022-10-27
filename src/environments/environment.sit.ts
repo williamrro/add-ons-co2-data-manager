a767d275dfefe67cb15a5d2533f35bb9adf4ffe5
raghavendra paluchuru <raghavendra.paluchuru@traxtech.com>
2022-10-27 17:54:22 +0530

    sit Env backend Url Update

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "http://app-ttsm-002-dev-003.use1-nprd-001.traxgrp.net:9050"
+    apiUrl: "http://wapp-ttsm-002-sit-003.use1-nprd-001.traxgrp.net:9050"
 };
