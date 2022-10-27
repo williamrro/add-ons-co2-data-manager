666b2ebdedaa7d0533a5135f309f0a62ffc4e5e5
raghavendra paluchuru <raghavendra.paluchuru@traxtech.com>
2022-10-27 20:08:07 +0530

    Routing File changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,15 +4,17 @@ import { DTComponent } from "../app/datatable/datatable.component";
 import { environment } from "../environments/environment";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: DTComponent
-  }
+  // {
+  //   path: "",
+  //   component: DTComponent
+  // },
+  { path: '', redirectTo: '/co2_ecm_ui', pathMatch: 'full' },
+  { path: 'co2_ecm_ui', component: DTComponent },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { enableTracing: false, useHash: true })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
