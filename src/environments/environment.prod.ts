3aee3ad0fb9a98e033d47e82691adb995e80a717
user namebalamurali.vegunta <balamurali.vegunta@traxtech.com>
2023-03-02 18:21:11 +0530

    Co2 apiUrl changes in prod env file & sidebar api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   // apiUrl: "//wapp-sit-01.mem-tw.veraction.net:7971/paymentTerms"
-  apiUrl: "//10.110.221.40:7971/closeMgnt/"
+  // apiUrl: "//10.110.221.40:7971/closeMgnt/"
+  apiUrl: "https://ttsm-002.traxtech.com/"
+
 };
  
