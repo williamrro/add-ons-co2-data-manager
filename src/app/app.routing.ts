afc55f702e03f948cc4b1f712d321a30c536c242
jayakrishnan-kaneria <jaya.krishnan@kanerika.com>
2024-07-02 19:14:18 +0530

    BLUE-13857: Updated FPS UI path parameter

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DTComponent } from './components/fps/datatable/datatable.component';
-import { environment } from '../environments/environment';
 
 const routes: Routes = [
-	// {
-	//   path: "",
-	//   component: DTComponent
-	// },
-	{ path: '', redirectTo: '/co2_fps_ui', pathMatch: 'full' },
-	{ path: 'co2_fps_ui', component: DTComponent },
+	{ path: '', redirectTo: '/co2_fps_dashboard', pathMatch: 'full' },
+	{ path: 'co2_fps_dashboard', component: DTComponent },
 ];
 
 @NgModule({
-	imports: [
-		//RouterModule.forRoot(routes)
-		RouterModule.forRoot(routes, { enableTracing: false, useHash: true }),
-	],
+	imports: [RouterModule.forRoot(routes, { enableTracing: false, useHash: true })],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
