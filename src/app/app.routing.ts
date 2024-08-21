755541eba19a7f5a57b0cc7f619ca9dd4aaa3303
Naresh reddy kayathi <naresh.kayathi@traxtech.com>
2024-08-21 23:16:20 +0530

    App routing build errors fixed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { LandingComponent } from './shared/landing/landing.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
+import { LandingComponent } from "./shared/landing/landing.component";
 
 const routes: Routes = [
 	{ path: 'application', component: LandingComponent },
@@ -9,13 +9,13 @@ const routes: Routes = [
 		path: 'fps',
 		canLoad: [AuthGuard],
 		canActivate: [AuthGuard],
-		loadChildren: () => import('./components/fps/fps.module').then((m) => m.FpsModule),
+		loadChildren: './components/fps/fps.module#FpsModule', // Angular 5-compatible syntax
 	},
 	{
 		path: 't4',
 		canLoad: [AuthGuard],
 		canActivate: [AuthGuard],
-		loadChildren: () => import('./components/t4/t4.module').then((m) => m.T4Module),
+		loadChildren: './components/t4/t4.module#T4Module', // Angular 5-compatible syntax
 	},
 	{ path: '', redirectTo: 'application', pathMatch: 'full' },
 	{ path: '**', redirectTo: 'application', pathMatch: 'full' },
