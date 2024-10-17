b0b8c8ecdfe9280f115f65cbc9c0b7a8859d5f05
Naresh reddy kayathi <naresh.kayathi@traxtech.com>
2024-10-17 15:38:27 +0530

    BLUE-14350 Automatic routing based on client access changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,78 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { ISubscription } from 'rxjs/Subscription';
-import { Broadcaster } from './shared/broadcaster';
-import { SearchService } from './services/search.service';
-import { AuthGuard } from './guards/auth.guard';
+import { Component } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { ISubscription } from "rxjs/Subscription";
+import { Broadcaster } from "./shared/broadcaster";
+import { SearchService } from "./services/search.service";
+import { AuthGuard } from "./guards/auth.guard";
+import { AppService } from "./app.service";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-	title = 'app';
-	public rlink: any;
-	public ractlink: any;
-	public svgLoader: boolean = false;
-	public mask: boolean = false;
-	private svgloaderB: any;
-	private maskloaderB: any;
+  title = "app";
+  public rlink: any;
+  public ractlink: any;
+  public svgLoader: boolean = false;
+  public mask: boolean = false;
+  private svgloaderB: any;
+  private maskloaderB: any;
+  isT4User: boolean = false;
+  routerSub$: ISubscription;
+  accessList: any = [];
+  constructor(
+    private router: Router,
+    private searchService: SearchService,
+    private authGuard: AuthGuard,
+    private broadcaster: Broadcaster,
+    private appService: AppService
+  ) {
+  }
 
-	isT4User: boolean = false;
-	routerSub$: ISubscription;
+  ngOnInit() {
+    this.svgloaderB = this.broadcaster
+      .on<string>("svgLoader")
+      .subscribe((isVisible: any) => {
+        this.svgLoader = isVisible;
+      });
+    this.maskloaderB = this.broadcaster
+      .on<boolean>("mask")
+      .subscribe((isMask: boolean) => {
+        this.mask = isMask;
+      });
+    this.routerSub$ = this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((route: any) => {
+        const { url = "" } = route || {};
+        const isT4Route = url.includes("/t4");
+        this.isT4User = isT4Route;
+        if (isT4Route)
+          this.searchService.setCurrentT4Tab(/[^/]*$/.exec(url)[0]);
+      });
 
-	constructor(
-		private router: Router,
-		private searchService: SearchService,
-		private authGuard: AuthGuard,
-		private broadcaster: Broadcaster
-	) {}
+    // In case of authentication for FPS and T4 applications, use below code to set access (first param for FPS and second for T4)
+    this.appService.redirectCo2App().subscribe((res) => {
+      if (res) {
+        this.accessList = res;
+        this.authGuard.setAccessInfo(
+          this.accessList.includes("FPS"),
+          this.accessList.includes("TTSM")
+        );
+      }
+    });
+  }
 
-	ngOnInit() {
-		this.svgloaderB = this.broadcaster.on<string>('svgLoader').subscribe((isVisible: any) => {
-			this.svgLoader = isVisible;
-		});
-		this.maskloaderB = this.broadcaster.on<boolean>('mask').subscribe((isMask: boolean) => {
-			this.mask = isMask;
-		});
-		this.routerSub$ = this.router.events
-			.filter((event) => event instanceof NavigationEnd)
-			.subscribe((route: any) => {
-				const { url = '' } = route || {};
-				const isT4Route = url.includes('/t4');
-				this.isT4User = isT4Route;
-				if (isT4Route) this.searchService.setCurrentT4Tab(/[^/]*$/.exec(url)[0]);
-			});
-
-		// In case of authentication for FPS and T4 applications, use below code to set access (first param for FPS and second for T4)
-		// this.authGuard.setAccessInfo(true, false);
-	}
-
-	ngOnDestroy() {
-		if (this.svgloaderB) {
-			this.svgloaderB.unsubscribe();
-		}
-		if (this.maskloaderB) {
-			this.maskloaderB.unsubscribe();
-		}
-		if (this.routerSub$) {
-			this.routerSub$.unsubscribe();
-		}
-	}
+  ngOnDestroy() {
+    if (this.svgloaderB) {
+      this.svgloaderB.unsubscribe();
+    }
+    if (this.maskloaderB) {
+      this.maskloaderB.unsubscribe();
+    }
+    if (this.routerSub$) {
+      this.routerSub$.unsubscribe();
+    }
+  }
 }
--- src/app/shared/landing/landing.component.ts
+++ src/app/shared/landing/landing.component.ts
@@ -1,38 +1,48 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ISubscription } from 'rxjs/Subscription';
-import { AuthGuard } from '../../guards/auth.guard';
+import { Component, HostBinding, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ISubscription } from "rxjs/Subscription";
+import { AuthGuard } from "../../guards/auth.guard";
+import { AppService } from "../../app.service";
 
 @Component({
-	selector: 'app-landing',
-	templateUrl: './landing.component.html',
-	styleUrls: ['./landing.component.scss'],
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
-	@HostBinding('class') class = 'autoFlexColumn';
+  @HostBinding("class") class = "autoFlexColumn";
 
-	hasFpsAccess: boolean = true;
-	hasT4Access: boolean = true;
-	accessSub$: ISubscription;
+  hasFpsAccess: boolean = true;
+  hasT4Access: boolean = true;
+  accessSub$: ISubscription;
 
-	constructor(private router: Router, private authGuard: AuthGuard) {}
+  constructor(
+    private router: Router,
+    private authGuard: AuthGuard,
+  ) {}
 
-	ngOnInit() {
-		this.accessSub$ = this.authGuard.getAccessInfo.subscribe((info: any) => {
-			const { fpsAccess, t4Access } = info;
-			this.hasFpsAccess = fpsAccess;
-			this.hasT4Access = t4Access;
-		});
-	}
+  ngOnInit() {
+    this.accessSub$ = this.authGuard.getAccessInfo.subscribe((info: any) => {
+      const { fpsAccess, t4Access } = info;
+      this.hasFpsAccess = fpsAccess;
+      this.hasT4Access = t4Access;
+      console.log(info);
+      if (this.hasFpsAccess && this.hasT4Access) {
+        this.hasFpsAccess = this.hasT4Access = true;
+      } else if (this.hasFpsAccess || this.hasT4Access) {
+        this.onNavigate(this.hasFpsAccess);
+      }
+    });
+  }
 
-	onNavigate(isFps: boolean) {
-		if (isFps) this.router.navigateByUrl('/fps');
-		else this.router.navigateByUrl('/t4');
-	}
+  onNavigate(isFps: boolean) {
+    if (isFps) this.router.navigateByUrl("/fps");
+    else this.router.navigateByUrl("/t4");
+  }
 
-	ngOnDestroy() {
-		if (this.accessSub$) {
-			this.accessSub$.unsubscribe();
-		}
-	}
+  ngOnDestroy() {
+    if (this.accessSub$) {
+      this.accessSub$.unsubscribe();
+    }
+  }
 }
