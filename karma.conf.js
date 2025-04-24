f94527739d1ca110e03131cbcbb702237582413c
Balamurali Vegunta <balamurali.vegunta@traxtech.com>
2025-04-25 00:33:35 +0530

    CYAN-1960 Co2 manager-ui - Angular Version Upgrade from 5-6-7 for UI applications

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -19,7 +19,7 @@ module.exports = function (config) {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    angularCli: {
+    angular: {
       environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
